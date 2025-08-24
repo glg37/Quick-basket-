d01222af435234c252fe28b8f42ea0d01f3feabd
glg37 <bielgutjahr1@gmail.com>
2025-08-24 11:02:23 -0300

    wind

---
+++ Quick Basket/Assets/scripts/WindZone.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WindZone : MonoBehaviour
+{
+    [Header("Força do Vento")]
+    public float windForce = 2f; // positivo = direita, negativo = esquerda
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            rb.AddForce(Vector2.right * windForce);
+        }
+    }
+}
